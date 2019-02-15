f160b0c70703e0d7ca845783a2837b5667f06043
bagsolo <a06nsn@gmail.com>
2019-02-16 05:42:44 +0600

    Lab 3

--- Week 2/Task4/Task4/Task4/Program.cs
+++ Week 2/Task4/Task4/Task4/Program.cs
@@ -11,47 +11,41 @@ namespace TestCopyCreateDelete
     {
         static void Main(string[] args)
         {
-            string FileName = "text.txt";//Даём имя файлу 
-            string sourcePath = @"C:\Users\PC\Desktop\PP2\Week 2\Task4\path";//Путь к папке источнику
-            string targetPath = @"C:\Users\PC\Desktop\PP2\Week 2\Task4\path1";//Путь к папке в которую надо переместить
+            string FileName = "text.txt";
+            string sourcePath = @"C:\Users\PC\Desktop\PP2\Week 2\Task4\path";
+            string targetPath = @"C:\Users\PC\Desktop\PP2\Week 2\Task4\path1";
 
-            string sourceFile = System.IO.Path.Combine(sourcePath, FileName);//Связываю файл с источником
-            string destFile = System.IO.Path.Combine(targetPath, FileName);//и с папкой в которую надо переместить
+            string sourceFile = System.IO.Path.Combine(sourcePath, FileName);
+            string destFile = System.IO.Path.Combine(targetPath, FileName);
 
-            if (!System.IO.Directory.Exists(targetPath))//Услвоия если папки не существует
+            if (System.IO.Directory.Exists(sourcePath))
             {
-                System.IO.Directory.CreateDirectory(targetPath);
-            }
-            System.IO.File.Copy(sourceFile, destFile, true);//Копируем папку
-
-            if (System.IO.Directory.Exists(sourcePath))//Услвоия для перемещения файла с папки в другую папку
-            {
-                string[] files = System.IO.Directory.GetFiles(sourcePath);//Массив для файла
+                string[] files = System.IO.Directory.GetFiles(sourcePath);
                 
-                foreach (string s in files)//Для копирования файла и перезаписи если он уже существует
+                foreach(string s in files)
                 {
-                    FileName = System.IO.Path.GetFileName(s);//Извлекаем имя файла из пути
+                    FileName = System.IO.Path.GetFileName(s);
                     destFile = System.IO.Path.Combine(targetPath, FileName);
                     System.IO.File.Copy(s, destFile, true);
                 }
             }
-            if (System.IO.File.Exists(@"C:\Users\PC\Desktop\PP2\Week 2\Task4\path\text.txt"))
+            if(System.IO.File.Exists(@"C:\Users\PC\Desktop\PP2\Week 2\Task4\path\text.txt"))
             {
                 try
                 {
-                    System.IO.File.Delete(@"C:\Users\PC\Desktop\PP2\Week 2\Task4\path\text.txt");//Удаление файла
+                    System.IO.File.Delete(@"C:\Users\PC\Desktop\PP2\Week 2\Task4\path\text.txt");
                 }
-                catch (System.IO.IOException e)//Для показа ошибки если файла не будет
+                catch
                 {
-                    Console.WriteLine(e.Message);
+                    Console.WriteLine("Error file does not exist");
                     return;
                 }
             }
             else
             {
-                Console.WriteLine("Source path does not exist");//Если папки источника не будет существовать
+                Console.WriteLine("Source path does not exist");
             }
-            Console.ReadKey();//Для того чтобы консоль сразу не закрывалась
+            Console.ReadKey();
         }
     }
 }
---
+++ Week 3/FarManager/FarManager/Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1
+{
+    class FarManager
+    {
+        DirectoryInfo dir = null;
+        public int cursor;
+        public string path;
+        public int size;
+        FileSystemInfo f1 = null;
+       
+        public FarManager(string path) // Конструктор который получает путь и записывает его в исходный путь
+        {
+            this.path = path;
+            cursor = 0;
+        }
+        public void Color(FileSystemInfo f, int index) // Метод для обозначения разных типов данных разными цветами
+        {
+            if (cursor == index) // Меняет цвет места где находится курсор
+            {
+                Console.BackgroundColor = ConsoleColor.Magenta;
+                Console.ForegroundColor = ConsoleColor.White;
+                f1 = f; // Присваеваемзначение чтобы мы могли использовать переменную f1
+            }
+            else if (f.GetType() == typeof(DirectoryInfo)) // Цвет для Папки
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else // Цвета для остальных
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+        }
+        public void Show() // Метод вписывающий нашу папку
+        {
+
+            dir = new DirectoryInfo(path); // Назначает путь
+            FileSystemInfo[] FSI = dir.GetFileSystemInfos(); // Записывает всю информацию про папки и файлы в массив
+            Console.BackgroundColor = ConsoleColor.Black;//Постоянно меняет цвет нашего фона
+            Console.Clear();
+            for (int i = 0, j = 0; i < FSI.Length; i++) // Цикл показывающий всё что есть в папке
+            {
+                if (FSI[i].Name[0] == '.') // Не показывать скрытые файлы
+                    continue;
+                Color(FSI[i], j); // Вызывает функцию которая перекрашывает наши папки и файлы
+                Console.WriteLine(j + 1 + ". " + FSI[i].Name); // Нумерует и показывает файлы и папки
+                j++;
+            }
+
+        }
+        public void HiddenFiles() // Метод вычисляющий скрытые файлы
+        {
+            DirectoryInfo d = new DirectoryInfo(path); // Посылает путь
+            FileSystemInfo[] fi = d.GetFileSystemInfos(); // Создаёт массив из папок и файлов
+            size = fi.Length; // размер равен всем элементам 
+            for (int i = 0; i < fi.Length; i++) // Цикл для нахождения скрытых файлов
+            {
+                if (fi[i].Name[0] == '.') // Уменьшаем размер если мы находим скрытые файлы
+                    size--;
+            }
+        }
+        public void Down() // Метод для перехода вниз
+        {
+            cursor++;
+            if (cursor == size) // 
+                cursor = 0;
+        }
+        public void Up()//Метод для перехода вверх
+        {
+            cursor--;
+            if (cursor < 0) // 
+                cursor = size - 1;
+        }
+        public void Start() //Главная функция
+        {
+
+            ConsoleKeyInfo Cons;
+            bool Ok = true; // Булеаная из-за которой наша консоль остаётся открытой
+            while (Ok == true) //Цикл зависящий от булеаной
+            {
+                HiddenFiles(); // Размер без скрытых файлов
+                Show(); // Показывает все элементы в массиве 
+                Cons = Console.ReadKey(); //Кнопка с определёнными биндами
+                if (Cons.Key == ConsoleKey.DownArrow) // Вызов метода Down нажатием клавишей вниз
+                {
+                    Down();
+                }
+                else if (Cons.Key == ConsoleKey.Q) // Выход из консоли нажатием Q
+                {
+                    Ok = false;
+                }
+                else if (Cons.Key == ConsoleKey.UpArrow) // Вызов метода Up нажатием клавишы вверх
+                {
+                    Up();
+                }
+                else if (Cons.Key == ConsoleKey.Enter) // Открываем папку или файл нажатием ENTER
+                {
+                    if (f1.GetType() == typeof(DirectoryInfo)) // Проверка типа файла
+                    {
+                        cursor = 0;
+                        path = f1.FullName; // Путь приравнивается пути этой папки
+                    }
+                    else
+                    {
+                        StreamReader SR = new StreamReader(f1.FullName);
+                        Console.WriteLine(SR.ReadToEnd());// Показвает все что есть в файле
+                        SR.Close(); 
+                        Console.ReadKey();
+                        Console.Clear();
+
+                    }
+                }
+                else if (Cons.Key == ConsoleKey.Escape) // Идём назад если нажимется клавиша Escape
+                {
+                    if (dir.Parent.FullName != @"C:\") //Ограничение до диска C
+                    {
+                        path = dir.Parent.FullName;//Обновляет наш путь
+                        cursor = 0;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("You Can't go out from the disk"); // Если дошёл до диска С и нажмаешь Escape
+                    }
+                }
+                else if (Cons.Key == ConsoleKey.Backspace) //Удаление
+                {
+                    if (f1.GetType() == typeof(DirectoryInfo))
+                    {
+                        cursor = 0;
+                        Directory.Delete(f1.FullName, true);
+                    }
+                    else
+                    {
+                        cursor = 0;
+                        File.Delete(f1.FullName);
+                    }
+                }
+                else if (Cons.Key == ConsoleKey.Tab) // Переминеование
+                {
+                    Console.Clear();
+                    string name = Console.ReadLine(); // Новое имя
+                    Console.Clear();
+                    string copPath = Path.Combine(dir.FullName, name); //Создаём путь 
+                    if (f1.GetType() == typeof(DirectoryInfo))
+                    {
+                        Directory.Move(f1.FullName, copPath);
+                    }
+                    else
+                    {
+                        File.Move(f1.FullName, copPath);
+                    }
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\PC\Desktop";
+            FarManager FM = new FarManager(path);
+            FM.Start(); // Вызываем функцию
+        }
+    }
+}
